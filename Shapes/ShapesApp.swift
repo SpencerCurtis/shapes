7234babd0b7860510400b6b759d56a5ca1e25c7a
Spencer Curtis <spencercolecurtis@gmail.com>
2025-05-04 11:21:41 -0600

    Initial Commit

---
+++ Shapes/ShapesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShapesApp.swift
+//  Shapes
+//
+//  Created by Spencer Curtis on 5/4/25.
+//
+
+import SwiftUI
+
+@main
+struct ShapesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ShapesTests/ShapesTests.swift
@@ -0,0 +1,17 @@
+//
+//  ShapesTests.swift
+//  ShapesTests
+//
+//  Created by Spencer Curtis on 5/4/25.
+//
+
+import Testing
+@testable import Shapes
+
+struct ShapesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
